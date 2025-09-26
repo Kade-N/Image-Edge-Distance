c651aaedb34048f355061181b035d7ed05cb950b
Kade <kade@aceofkade.com>
2025-09-26 06:09:26 -0400

    major update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System.Drawing;
 
+// Returns true if this color should remain as-is.
+// Modify to change which pixels are considered "fixed".
+// The distance will be calculated to the nearest fixed pixel.
+static bool IsFixedColor(Color color)
+{
+    // Black pixels
+    return color.R == 0 && color.G == 0 && color.B == 0;
+}
+
+// Returns color based on distance value.
+// Modify to change the color gradient.
+static Color GetResultColor(int value)
+{
+    // Green to white
+    return Color.FromArgb(value, 255, value);
+}
+
+// Divides distance value over a larger area, making the effect more gradual.
+// Increase to make the effect more gradual, decrease to make it sharper.
+const int DIVISOR = 2;
+
+
+
 // Get first argument
 var fileName = args.First();
-Console.WriteLine($"Opening image {fileName}");
+Console.WriteLine($"Processing image {fileName}");
 
 // Load image
 var image = new Bitmap(fileName);
 int totalPixels = image.Width * image.Height;
+
+
 
 // Array to hold distances
 int[,] distances = new int[image.Width, image.Height];
@@ -16,9 +41,9 @@ for (int y = 0; y < image.Height; y++)
 {
     for (int x = 0; x < image.Width; x++)
     {
-        // Set initial distance to 0 for black pixels, max for others
+        // Set initial distance to 0 for fixed pixels, max for others
         var pixel = image.GetPixel(x, y);
-        distances[x, y] = pixel.R == 0 && pixel.G == 0 && pixel.B == 0 ? 0 : int.MaxValue;
+        distances[x, y] = IsFixedColor(pixel) ? 0 : int.MaxValue;
     }
 }
 
@@ -27,13 +52,20 @@ for (int y = 1; y < image.Height; y++)
 {
     for (int x = 1; x < image.Width; x++)
     {
-        // Skip black pixels
+        // Skip fixed pixels
         if (distances[x, y] == 0) continue;
 
         // Check pixel to left
-        distances[x, y] = Math.Min(distances[x, y], distances[x - 1, y] + 1);
+        if (distances[x - 1, y] < int.MaxValue)
+        {
+            distances[x, y] = Math.Min(distances[x, y], distances[x - 1, y] + 1);
+        }
+
         // Check pixel above
-        distances[x, y] = Math.Min(distances[x, y], distances[x, y - 1] + 1);
+        if (distances[x, y - 1] < int.MaxValue)
+        {
+            distances[x, y] = Math.Min(distances[x, y], distances[x, y - 1] + 1);
+        }
     }
 }
 
@@ -42,13 +74,20 @@ for (int y = image.Height - 2; y >= 0; y--)
 {
     for (int x = image.Width - 2; x >= 0; x--)
     {
-        // Skip black pixels
+        // Skip fixed pixels
         if (distances[x, y] == 0) continue;
 
         // Check pixel to right
-        distances[x, y] = Math.Min(distances[x, y], distances[x + 1, y] + 1);
+        if (distances[x + 1, y] < int.MaxValue)
+        {
+            distances[x, y] = Math.Min(distances[x, y], distances[x + 1, y] + 1);
+        }
+
         // Check pixel below
-        distances[x, y] = Math.Min(distances[x, y], distances[x, y + 1] + 1);
+        if (distances[x, y + 1] < int.MaxValue)
+        {
+            distances[x, y] = Math.Min(distances[x, y], distances[x, y + 1] + 1);
+        }
     }
 }
 
@@ -60,11 +99,13 @@ for (int y = 0; y < image.Height; y++)
         int distance = distances[x, y];
         if (distance > 0 && distance < int.MaxValue)
         {
-            int value = Math.Min(distance / 2, 255);
-            image.SetPixel(x, y, Color.FromArgb(255, value, value));
+            int value = Math.Min(distance / DIVISOR, 255);
+            image.SetPixel(x, y, GetResultColor(value));
         }
     }
 }
+
+
 
 // Save image
 var outputFileName = Path.GetFileNameWithoutExtension(fileName) + "_output.png";
---
+++ README.md
@@ -0,0 +1,77 @@
+# Image Edge Distance
+This is a simple program to draw gradients on images based on the distance from any "fixed colour" pixel. This is better explained through the following example images.
+
+## Example
+This is used as the input image. It has a black background with white shapes.
+
+![Example input, an image with a black background and white shapes](examples/example.png)
+
+The "fixed color" is the black pixels. The non-fixed pixels (i.e. the white pixels) will have a gradient drawn over them, based on the distance from any fixed (black) pixel. The gradient is green to white, so pixels closest to a fixed (black) pixel become green, and as the distance from any fixed (black) pixel increases, the pixels become whiter.
+
+![Example output, where the previously white areas now have a green to white gradient](examples/example_output.png)
+
+## Example 2
+The resulting image from the previous example is now used as the input image. The fixed pixels are now set to any pixel containing green, which means that any black pixel is not fixed. The gradient is blue to black.
+
+![Example output 2, where the previously black areas now have a blue to black gradient](examples/example_output_output.png)
+
+## Usage
+The code is intended to be modified so that you can configure the fixed colours and the gradient.
+
+### Configuration
+#### Fixed colours
+The fixed colour will not be modified by the program. Any pixels satisfying this condition will be used in distance calculations.
+
+Here are example conditions corresponding to the above examples.
+```cs
+static bool IsFixedColor(Color color)
+{
+    // Black pixels
+    return color.R == 0 && color.G == 0 && color.B == 0;
+}
+```
+```cs
+static bool IsFixedColor(Color color)
+{
+    // Any green pixels
+    return color.G > 0;
+}
+```
+
+#### Result colours
+The result colour is used to draw the gradient. The distance value is passed in, and will always be a number 1-255 inclusive.
+
+Here are example gradients corresponding to the above examples.
+```cs
+static Color GetResultColor(int value)
+{
+    // Green to white
+    return Color.FromArgb(value, 255, value);
+}
+```
+```cs
+static Color GetResultColor(int value)
+{
+    // Blue to black
+    return Color.FromArgb(0, 0, 255 - value);
+}
+```
+
+#### Divisor
+As the RGB range is limited to 0-255 inclusive, this means that a gradient normally cannot extend beyond 256 pixels.
+
+To get around this limitation, you can set a divisor, which stretches the gradient.
+
+For example...
+1 = gradient drawn over 256 pixels
+2 = gradient drawn over 512 pixels
+4 = gradient drawn over 1024 pixels
+
+### Running
+Use .NET 9 on Windows to run this program. The easiest way is to have the .NET SDK installed, and use `dotnet run <file name>` while in the folder containing the program code. The resulting image will have `_output` appended to its filename.
+
+No other steps are needed to build the program - just clone this repo, edit the code as explained above, and run this command.
+
+For example, `dotnet run image.png`. The output image will be called `image_output.png`.
+
+It may take more than a minute to process very large images.
